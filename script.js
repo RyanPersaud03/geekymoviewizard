8f13a3eef9d3700e5902f08deb91d8b50a843b76
mk10100 <mohamedkhalil10100@gmail.com>
2023-09-28 21:29:30 -0400

    mk-htmlskelton

--- script.js
+++ script.js
@@ -1,71 +1,109 @@
-//variables & objects
-var userAnswer = [];
+// Variables & objects
+var userAnswers = [];
 var questionIndex = 0;
-let currentQuestion = 0;
 
-//Wizard Generator Questions. 
-let question = [
-    {
-        question: 'What are your favourite movie genres?'
-        options: ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
-    },
-    {
-        question: 'Do you watch foreign films?'
-        options: ['Yes', 'No']
-    },
-    {
-        question: 'Do you prefer animated or live-action films?'
-        options: ['Animated', 'Live-Action']
-    },
-    {
-        question: 'Which are your favourite actors?'
-        options: ['Brad Pitt', 'Adam Sandler', 'Leonardo DiCaprio', 'Leonardo DiCaprio', 'Samuel L.Jackson', 'Chris Hemsworth', 'Dwayne Johnson']
-    },
-    {
-        question: 'Which are your favourite actresses?'
-        options: ['Scarlett Johansson', 'Jennifer Lawrence', 'Meryl Streep', 'Viola Davis', 'Amy Adams', 'Emma Stone']
-    },
-    {
-        question: 'Based on below rating system, describe your mood today.'
-        options: ['']
-    }
+// Wizard Generator Questions
+var questions = [
+  {
+    question: "What are your favorite movie genres?",
+    options: [
+      "Action",
+      "Adventure",
+      "Animation",
+      "Biography",
+      "Comedy",
+      "Crime",
+      "Documentary",
+      "Drama",
+      "Family",
+      "Fantasy",
+      "Film-Noir",
+      "History",
+      "Horror",
+      "Music",
+      "Musical",
+      "Mystery",
+      "Romance",
+      "Sci-Fi",
+      "Short",
+      "Sport",
+      "Thriller",
+      "War",
+      "Western",
+    ],
+  },
+  {
+    question: "Do you watch foreign films?",
+    options: ["Yes", "No"],
+  },
+  {
+    question: "Do you prefer animated or live-action films?",
+    options: ["Animated", "Live-Action"],
+  },
+  {
+    question: "Which are your favorite actors?",
+    options: [
+      "Brad Pitt",
+      "Adam Sandler",
+      "Leonardo DiCaprio",
+      "Samuel L. Jackson",
+      "Chris Hemsworth",
+      "Dwayne Johnson",
+    ],
+  },
+  {
+    question: "Which are your favorite actresses?",
+    options: [
+      "Scarlett Johansson",
+      "Jennifer Lawrence",
+      "Meryl Streep",
+      "Viola Davis",
+      "Amy Adams",
+      "Emma Stone",
+    ],
+  },
+  {
+    question: "Based on the rating system, describe your mood today.",
+    options: ["Happy", "Sad", "Excited", "Relaxed"],
+  },
 ];
 
-//function to display the question
+// Function to display a question
 function showQuestion(questionIndex) {
-    var questionContainer = document.getElementById('question-container');
-    var optionsContainer = document.getElementById('options-container');
+  var questionContainer = document.getElementById("question-container");
+  var optionsContainer = document.getElementById("options-container");
 
-    //Display question
-    questionContainer.innerHTML = questions[questionIndex].question;
+  // Display the question
+  questionContainer.textContent = questions[questionIndex].question;
 
-    //Display options
-    optionsContainer.innerHTML = '';
-    for (var i = 0; i < questions[questionIndex].options.length; i++) {
-        var option = document.createElement('button');
-        option.innerHTML = questions[questionIndex].options[i];
-        option.addEventListener('click', handleOptionClick);
-        optionsContainer.appendChild(option);
-    }
+  // Display answer options
+  optionsContainer.innerHTML = "";
+  for (var i = 0; i < questions[questionIndex].options.length; i++) {
+    var option = document.createElement("button");
+    option.textContent = questions[questionIndex].options[i];
+    option.addEventListener("click", handleOptionClick);
+    optionsContainer.appendChild(option);
+  }
 }
+
 // Event handler for option click
 function handleOptionClick(event) {
-    var selectedOption = event.target.innerHTML;
-    userAnswers.push(selectedOption);
+  var selectedOption = event.target.textContent;
+  userAnswers.push(selectedOption);
 
-    // Move to the next question or show the final result
-    questionIndex++;
-    if (questionIndex < questions.length) {
-        showQuestion(questionIndex);
-    } else {
-        displayUserAnswers();
-    }
+  // Move to the next question or show the final result
+  questionIndex++;
+  if (questionIndex < questions.length) {
+    showQuestion(questionIndex);
+  } else {
+    displayUserAnswers();
+  }
 }
 
-// Function to display user answers
+// Function to display user answers/result
 function displayUserAnswers() {
-    var resultContainer = document.getElementById('result-container');
-    resultContainer.innerHTML = 'User Answers: ' + userAnswers.join(', ');
+  var resultContainer = document.getElementById("result-container");
+  resultContainer.textContent = "User Answers: " + userAnswers.join(", ");
 }
 
 // Initialize by showing the first question
